25d0ec5f8c29483c7449480f7aa40da95e0a98ad
AmishMishra11 <amishmishra11@gmail.com>
2022-03-19 20:03:07 +0530

    fix test 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import Category from "../../Components/Category";
 import Carousel from "../../Components/Carousel";
 import Popular from "../../Components/Popular";
 
-export default function Home() {
+function Home() {
   return (
     <div className="home">
       <Category />
@@ -15,3 +15,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;

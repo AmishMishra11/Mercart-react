7cdd274061a7d1bb481730ff89d6acc03ad99ac3
AmishMishra11 <amishmishra11@gmail.com>
2022-03-19 20:12:24 +0530

    Revert "fix test 4"
    This reverts commit f50e161946a4cb8b3b418130b110a9cceca6c0b2.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./styles.css";
 
-// import Category from "../../Components/Category";
-// import Carousel from "../../Components/Carousel";
-// import Popular from "../../Components/Popular";
+import Category from "../../Components/Category";
+import Carousel from "../../Components/Carousel";
+import Popular from "../../Components/Popular";
 
 export function Home() {
   return (
     <div className="home">
-      <h1>Hello world</h1>
-      {/* <Category />
+      <Category />
 
       <Carousel />
-      <Popular /> */}
+      <Popular />
     </div>
   );
 }
